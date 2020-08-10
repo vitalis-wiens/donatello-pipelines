dbb22e82125d68fc9a8de80127e53a2e459d7d9f
Vitalis Wiens <vitalis.wiens@gmail.com>
2020-08-10 10:20:45 +0200

    Updates

---
+++ precache-manifest.8c088d25f6a4ceb09a4e9dc572e42d4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4b8db91ef6225bb9dfd9d94a9ac50be",
+    "url": "/donatello-test/index.html"
+  },
+  {
+    "revision": "97a1518edceb5c64ee1c",
+    "url": "/donatello-test/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "d74de80e23e6834ebc0e",
+    "url": "/donatello-test/static/css/main.62a34b28.chunk.css"
+  },
+  {
+    "revision": "97a1518edceb5c64ee1c",
+    "url": "/donatello-test/static/js/2.452f3b5d.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/donatello-test/static/js/2.452f3b5d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d74de80e23e6834ebc0e",
+    "url": "/donatello-test/static/js/main.b3c350f3.chunk.js"
+  },
+  {
+    "revision": "7e2f30fb7d4c13d052b8",
+    "url": "/donatello-test/static/js/runtime-main.8a99b99c.js"
+  }
+]);
